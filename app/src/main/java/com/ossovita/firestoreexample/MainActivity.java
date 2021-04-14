26e0d143388b130a8087599dc9cd50c32d26c7e6
yasineryigit <yasineryigit737@gmail.com>
2021-04-14 19:46:05 +0300

    First document

---
+++ app/src/main/java/com/ossovita/firestoreexample/MainActivity.java
@@ -0,0 +1,58 @@
+package com.ossovita.firestoreexample;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_DESCRIPTION = "description";
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
+
+    }
+
+
+    public void save_note(View v) {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+        Map<String, Object> note = new HashMap<>();
+        note.put(KEY_TITLE, title);
+        note.put(KEY_DESCRIPTION, description);
+        //db.document("Notebook/MyFirstNote"); bu şekilde de yazılabilir.
+        db.collection("Notebook").document("My First Note").set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(MainActivity.this, "Note saved successfully", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onFailure: " + e.getLocalizedMessage());
+            }
+        });
+    }
+}
